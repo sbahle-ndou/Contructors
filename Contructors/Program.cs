7bc98f2f5ba3f0bd0829757e3c3478ec3057fbc4
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 13:48:40 +0200

    Add project files.

---
+++ Contructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Porter", "JK Rowling", 400);
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
